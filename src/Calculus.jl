0aa775cc9888388c2ab0c7e1b2d1868dab5cd00b
jlvvlj <jimmylevagueresse@gmail.com>
2020-09-30 06:20:37 +0900

    commit

--- src/Calculus.jl
+++ src/Calculus.jl
@@ -2,15 +2,6 @@ module Calculus
 
 export interest, rate
 
-interest(amount, rate)
-
-function interest(amount, rate)
-    return amount * (1 + rate)
-end
-
-function rate(amount, interest)
-    return interest / amount
-end
 
 end
 
