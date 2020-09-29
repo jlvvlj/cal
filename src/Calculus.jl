3b45f3a4b18005cbc963969c596d4f9b7f9e21d0
jlvvlj <jimmylevagueresse@gmail.com>
2020-09-30 06:05:21 +0900

    Update Calculus.jl

--- src/Calculus.jl
+++ src/Calculus.jl
@@ -1,5 +1,17 @@
 module Calculus
 
-# Write your package code here.
+export interest, rate
+
+interest(amount, rate)
+
+export function interest(amount, rate)
+    return amount * (1 + rate)
+end
+
+export function rate(amount, interest)
+    return interest / amount
+end
+
+end
 
 end
